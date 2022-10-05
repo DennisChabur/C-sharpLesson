f7ac29c6f5d9fef2198ee112ea8bdf011315e61f
Den Chad <workplacel@mail.ru>
2022-10-05 11:51:09 +0500

    Task13

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
-Console.WriteLine("Input your XX number plz");
-System.Console.WriteLine(Convert.ToInt32(Console.ReadLine()) / 10);
+﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Input your XXX number plz");
+int thirdNumberInput = Convert.ToInt32(Console.ReadLine()) / 100;
+// if (thirdNumberInput == 0);
+// {
+//     System.Console.WriteLine("There is no 3rd number");
+// }
+if (thirdNumberInput == 0)
+{
+    System.Console.WriteLine("There's no 3rd number");
+}
+else
+{
+    System.Console.WriteLine("3rd number is " + thirdNumberInput);
+}
