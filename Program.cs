09271ad9b9358a2cd8901dd4024b45cd856498ac
Den <workplacel@mail.ru>
2022-10-07 01:51:15 +0500

    Task23

--- Program.cs
+++ Program.cs
@@ -2,22 +2,15 @@
 и находит расстояние между ними в 3D пространстве */
 
 Console.Clear();
+int N;
+System.Console.WriteLine("Input number of cubes");
+int.TryParse(System.Console.ReadLine()!, out N);
 
-int x1, y1, z1, x2, y2, z2;
-
-System.Console.WriteLine("input x1");
-int.TryParse(Console.ReadLine()!, out x1);
-System.Console.WriteLine("input y1");
-int.TryParse(Console.ReadLine()!, out y1);
-System.Console.WriteLine("input z1");
-int.TryParse(Console.ReadLine()!, out z1);
-System.Console.WriteLine("input x2");
-int.TryParse(Console.ReadLine()!, out x2);
-System.Console.WriteLine("input y2");
-int.TryParse(Console.ReadLine()!, out y2);
-System.Console.WriteLine("input z2");
-int.TryParse(Console.ReadLine()!, out z2);
-
-double cube = Math.Pow((x1-x2), 2)+Math.Pow((y1-y2), 2)+Math.Pow((z1-z2), 2);
-double L = Math.Sqrt(cube);
-System.Console.WriteLine(L);
+for (int i = 1; i <= N; i++)
+{
+    System.Console.Write($"{Math.Pow(i, 3)}");
+    if (i < N)
+    {System.Console.Write(", ");}
+    else
+    {continue;}
+}
