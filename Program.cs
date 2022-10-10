8aa03ddfddc33cb44b0aea4f9bd8c44f7fb425f2
Den <workplacel@mail.ru>
2022-10-10 23:48:55 +0500

    Task34 - at the beginning

--- Program.cs
+++ Program.cs
@@ -1,22 +1,30 @@
-﻿/* Задача 29: Напишите функцию, которая задаёт массив из 8 элементов и выводит их на экран.
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-6, 1, 33 -> [6, 1, 33] */
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int array = CreateArray()
 
 
-int[] array;
 
-array = Array();
-for (int i=0; i<array.Length; i++)
-    Console.Write($"{array[i]} ");
 
-int[] Array()
+
+
+
+// Create random array
+
+int[] CreateArray(int size, int min, int max)
 {
-    int[] _array = new int[8];
-    
-    for (int i = 0; i < 8; i++)
-    {
-        _array[i] = new Random().Next(0, 100);
-    }
-    return _array;
-    
+    int[] _array = new int[size];
+    for (int i = 0; i < size; i++)
+        _array[i] = new Random().Next(min, max);
+    return _array
+}
+
+void PrintArray(int[] _array)
+{
+    System.Console.Write("[");
+    foreach (var item in _array)
+        System.Console.Write($"{item} ");
+    System.Console.Write("]");
+    System.Console.WriteLine();
 }
