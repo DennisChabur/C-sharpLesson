5f9064d9e9d9dd032cef872dcdd71019eb0ebded
Den <workplacel@mail.ru>
2022-10-11 23:33:20 +0500

    Task38

--- Program.cs
+++ Program.cs
@@ -1,24 +1,25 @@
 ﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76 */
 
-double[] array = CreateArray(new Random().Next(0, 10), -1000, 1000);
+int[] array = CreateArray(new Random().Next(0, 10), -1000, 1000);
 
 PrintArray(array);
 
 
-double summ = 0;
-double max = 0;
-double min = 0;
-double temp = 0;
+int summ = 0;
+int max = array[0];
+int min = array[0];
 
 for (int i=0; i < array.Length; i++)
 {
-    if (array[i] < array[i+1])
-        temp = array[i];
-        
-    summ = max - min;
-}
-
+    if (array[i] > max)
+        max = array[i];
+    else if (array[i] < min)
+        min = array[i];
+    else continue;
+}      
+summ = max - min;
+Console.WriteLine($"{max} - {min} = {summ}");
 Console.WriteLine($"-> {summ}");
 
 
@@ -28,7 +29,7 @@ Console.WriteLine($"-> {summ}");
 
 // Create random array
 
-int[] CreateArray(int size, double min, double max)
+int[] CreateArray( int size, int min, int max)
 {
     int[] _array = new int[size];
     for (int i = 0; i < size; i++)
