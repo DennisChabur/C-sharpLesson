bd06bfb6755e7f3d8a8998b709e991280c1dd272
Den Chad <workplacel@mail.ru>
2022-10-11 13:09:33 +0500

    Task36

--- Program.cs
+++ Program.cs
@@ -1,19 +1,18 @@
-﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-Напишите программу, которая покажет количество чётных чисел в массиве.
-[345, 897, 568, 234] -> 2 */
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
 
-int[] array = CreateArray(new Random().Next(20), 100, 1000);
+int[] array = CreateArray(new Random().Next(0, 10), -1000, 1000);
 
 PrintArray(array);
 
-int count = 0;
-for (int i=0; i < array.Length; i++)
+int summ = 0;
+for (int i=1; i < array.Length; i+=2)
 {
-    if (array[i] % 2 ==0)
-        count ++;
+    summ += array[i];
 }
 
-Console.WriteLine($"-> {count}");
+Console.WriteLine($"-> {summ}");
 
 
 // Create random array
