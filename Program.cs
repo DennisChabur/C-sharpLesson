745088b025270bac8f0ed77e3d6ca4d4ae8826f1
Den <workplacel@mail.ru>
2022-10-13 10:20:47 +0500

    Task41 new try

--- Program.cs
+++ Program.cs
@@ -10,11 +10,14 @@ List<int> i = new List<int>();
 int num = 0;
 while (stop)                                                   // Цикл, с флагом
     System.Console.WriteLine("Input your number");      // Просим пользователя ввести число
-    if (int.TryParse(Console.ReadLine()!, out M))   // Если введенное можем перевести в int, то ...
+    int.TryParse(Console.ReadLine()!, out M);        // Если введенное можем перевести в int, то ...
+    
+    if (M)
     {
         if (M > 0) num += 1;                                   // Если М положительное, то счетчик +1
-        i.Add(M);                                        // В любом случае, добавляем значение М к списку
     }
-    else stop = false;                                         // Но если введено не число, то stop = false, значит зикл завершается
+    i.Add(M);                                            // В любом случае, добавляем значение М к списку
+    else 
+        stop = false;                                         // Но если введено не число, то stop = false, значит зикл завершается
 
 System.Console.WriteLine($"-> {num}");                  // Выводим счетчик
