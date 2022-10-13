3a04ed029759c473af3f41817054ee11ecd60cc5
Den <workplacel@mail.ru>
2022-10-14 00:40:07 +0500

    Task41 solved

--- Program.cs
+++ Program.cs
@@ -4,20 +4,21 @@
 using System;
 using System.Collections.Generic;
 
-int M;
-bool stop = true;
-List<int> i = new List<int>();
-int num = 0;
-while (stop)                                                   // Цикл, с флагом
-    System.Console.WriteLine("Input your number");      // Просим пользователя ввести число
-    int.TryParse(Console.ReadLine()!, out M);        // Если введенное можем перевести в int, то ...
-    
-    if (M)
-    {
-        if (M > 0) num += 1;                                   // Если М положительное, то счетчик +1
-    }
-    i.Add(M);                                            // В любом случае, добавляем значение М к списку
-    else 
-        stop = false;                                         // Но если введено не число, то stop = false, значит зикл завершается
-
-System.Console.WriteLine($"-> {num}");                  // Выводим счетчик
+System.Console.WriteLine("Input numbers of inputed numbers");
+int Num;
+int.TryParse(Console.ReadLine()!, out Num);
+int count = 0;
+// int i = 0;
+List<int> list = new List<int>();
+for ( int i = 0; i < Num; i++)
+{
+    System.Console.WriteLine("Input number");
+    int numInput;
+    int.TryParse(Console.ReadLine()!, out numInput);
+    if (numInput > 0)   count += 1;
+    list.Add(numInput);
+    // i ++;
+}
+foreach (var item in list)
+    System.Console.Write($"{item} ");
+System.Console.WriteLine($" -> {count}");
