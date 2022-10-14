694040756b0ce181d237644f8fc9f1f7cbc3b2fb
Den <workplacel@mail.ru>
2022-10-15 00:52:23 +0500

    Task50

--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
-﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-m = 3, n = 4.
-0,5 7 -2 -0,2
-1 -3,3 8 -9,9
-8 7,8 -7,1 9 */
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции 
+элемента в двумерном массиве, и возвращает значение этого элемента или 
+же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
 
 int m = 3;
 int n = 4;
-double[,] array = new double[m,n];
+int[,] array = new int[m,n];
 
 for (int i = 0; i < m; i++)
 {
     for (int j = 0; j < n; j++)
     {
-        array[i,j] = (double) new Random().Next(-100, 100)/10;
+        array[i,j] = (int) new Random().Next(10);
         Console.Write($"{array[i, j]} ");
     }
     Console.WriteLine();
+}
+
+int mInput, nInput;
+System.Console.WriteLine("Input m");
+int.TryParse(Console.ReadLine()!, out mInput);
+System.Console.WriteLine("Input n");
+int.TryParse(Console.ReadLine()!, out nInput);
+
+try
+{
+    System.Console.WriteLine($"{array[mInput,nInput]}");
+}
+catch (System.Exception)
+{
+    System.Console.WriteLine("Out of range");
 }
