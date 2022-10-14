b098518815d67fbbe0515792909fd39e2aa5f255
Den <workplacel@mail.ru>
2022-10-15 01:05:05 +0500

    Task52

--- Program.cs
+++ Program.cs
@@ -1,38 +1,29 @@
-﻿/* Задача 50. Напишите программу, которая на вход принимает позиции 
-элемента в двумерном массиве, и возвращает значение этого элемента или 
-же указание, что такого элемента нет.
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
 
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
-17 -> такого числа в массиве нет */
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
 
-int m = 3;
-int n = 4;
-int[,] array = new int[m,n];
+int m = 4, n = 5; 
+int[,] array = new int[m, n];  
+for (int i = 0; i < m; i++) 
+{     
+    for (int j = 0; j < n; j++)     
+    {         
+        array[i, j] = new Random().Next(10);         
+        Console.Write($"{array[i, j]} ");     
+    }     Console.WriteLine(); 
+} 
 
-for (int i = 0; i < m; i++)
+for (int j = 0; j < n; j++)
 {
-    for (int j = 0; j < n; j++)
+    int sum = 0;
+    for (int i = 0; i < m; i++)
     {
-        array[i,j] = (int) new Random().Next(10);
-        Console.Write($"{array[i, j]} ");
+        sum += array[i,j];
     }
-    Console.WriteLine();
-}
-
-int mInput, nInput;
-System.Console.WriteLine("Input m");
-int.TryParse(Console.ReadLine()!, out mInput);
-System.Console.WriteLine("Input n");
-int.TryParse(Console.ReadLine()!, out nInput);
-
-try
-{
-    System.Console.WriteLine($"{array[mInput,nInput]}");
-}
-catch (System.Exception)
-{
-    System.Console.WriteLine("Out of range");
+    System.Console.Write($"The average of summarize of {j} colomn is {(double)sum/m}\n");
 }
