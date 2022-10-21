9ca943f49c192278ff3cbfad81cd8151e42585c5
Den <workplacel@mail.ru>
2022-10-22 01:23:26 +0500

    Task60

--- Program.cs
+++ Program.cs
@@ -1,71 +1,64 @@
-﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-Например, даны 2 матрицы:
-2 4 | 3 4
-3 2 | 3 3
-Результирующая матрица будет:
-18 20
-15 18 */
+﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1) */
+
+int x=2, y=2, z=2;
+
+int[,,] array = new int[x, y, z];
+CreateArray(array);
+WriteArray(array);
 
 
-
-int m1 = 2, n1 = 3;
-int[,] array1 = new int[m1, n1];
-array1 = CreateArray(m1, n1);
-// PrintArray(array1);
-System.Console.WriteLine();
-
-int m2 = 3, n2 = 3;
-int[,] array2 = new int[m2, n2];
-array2 = CreateArray(m2, n2);
-// PrintArray(array2);
-System.Console.WriteLine();
-
-int[,] multiArray = new int[m1,n2];
-
-
-Console.WriteLine();
-for (int i = 0; i < m1; i++)
+void WriteArray (int[,,] array)
 {
-    for (int j = 0; j < n2; j++)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        
-        int sum = 0;
-        for (int k = 0; k < n1; k++)
-        {
-            sum += array1[i,k]*array2[k,j];
-        }
-        multiArray[i,j] = sum;
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        Console.Write( $"{array[i,j,k]} ({i},{j},{k}) ");
+      }
+      Console.WriteLine();
     }
+  }
 }
 
-PrintArray(multiArray);
-
-
-void PrintArray(int[,] _array)
+void CreateArray(int[,,] array)
 {
-    for (int i = 0; i < _array.GetLength(0); i++)
+  int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
     {
-        for (int j = 0; j < _array.GetLength(1); j++)
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
         {
-            Console.Write($"{_array[i,j]} ");
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
         }
-        System.Console.WriteLine();
+          number = temp[i];
+      }
     }
-}
-
-
-
-int[,] CreateArray(int size_m, int size_n, int min = 0, int max = 10)
-{
-    int[,] _array = new int[size_m, size_n];
-    for (int i = 0; i < _array.GetLength(0); i++)
+  }
+  int count = 0; 
+  for (int x = 0; x < array.GetLength(0); x++)
+  {
+    for (int y = 0; y < array.GetLength(1); y++)
     {
-        for (int j = 0; j < _array.GetLength(1); j++)
-        {
-            _array[i, j] = new Random().Next(min, max);
-            Console.Write($"{_array[i, j]} ");
-        }
-        Console.WriteLine();
+      for (int z = 0; z < array.GetLength(2); z++)
+      {
+        array[x, y, z] = temp[count];
+        count++;
+      }
     }
-    return _array;
+  }
 }
