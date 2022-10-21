dc2f1bfbe46497470ca490a73852f81540951592
Den <workplacel@mail.ru>
2022-10-22 01:35:26 +0500

    Task62

--- Program.cs
+++ Program.cs
@@ -1,64 +1,7 @@
-﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-Массив размером 2 x 2 x 2
-66(0,0,0) 25(0,1,0)
-34(1,0,0) 41(1,1,0)
-27(0,0,1) 90(0,1,1)
-26(1,0,1) 55(1,1,1) */
+﻿/* Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07 */
 
-int x=2, y=2, z=2;
-
-int[,,] array = new int[x, y, z];
-CreateArray(array);
-WriteArray(array);
-
-
-void WriteArray (int[,,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      for (int k = 0; k < array.GetLength(2); k++)
-      {
-        Console.Write( $"{array[i,j,k]} ({i},{j},{k}) ");
-      }
-      Console.WriteLine();
-    }
-  }
-}
-
-void CreateArray(int[,,] array)
-{
-  int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
-  int  number;
-  for (int i = 0; i < temp.GetLength(0); i++)
-  {
-    temp[i] = new Random().Next(10, 100);
-    number = temp[i];
-    if (i >= 1)
-    {
-      for (int j = 0; j < i; j++)
-      {
-        while (temp[i] == temp[j])
-        {
-          temp[i] = new Random().Next(10, 100);
-          j = 0;
-          number = temp[i];
-        }
-          number = temp[i];
-      }
-    }
-  }
-  int count = 0; 
-  for (int x = 0; x < array.GetLength(0); x++)
-  {
-    for (int y = 0; y < array.GetLength(1); y++)
-    {
-      for (int z = 0; z < array.GetLength(2); z++)
-      {
-        array[x, y, z] = temp[count];
-        count++;
-      }
-    }
-  }
-}
